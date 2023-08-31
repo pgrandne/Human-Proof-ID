1943817c757a26e245193f959ab7090ef84aafbb
pgrandne <perrin.grandne@protonmail.com>
2023-08-31 15:17:22 +0200

    test page deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Connected } from '../components/Connected'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export function Home() {
+export default function Home() {
   return (
     <>
       <div className="h-1/6 flex justify-between p-3">
@@ -16,16 +16,15 @@ export function Home() {
           alt="idcard"
         />
         <h1 className="text-5xl font-extrabold text-center mt-8 bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 via-red-400 to-indigo-400">Human Proof ID</h1>
-        <ConnectKitButton />
+        {/* <ConnectKitButton />
       </div>
       <div className="h-4/6 flex justify-center">
         <Connected>
           <Card />
         </Connected>
       </div>
-      <ToastContainer position="bottom-right" />
-    </>
-  )
+      <ToastContainer position="bottom-right" /> */}
+      </>
+      )
 }
 
-export default Home
